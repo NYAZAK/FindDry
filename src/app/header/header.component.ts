ec71e8e783b626efe79f2afe3e0ce1ce529716fe
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-02 17:23:01 +0100

    refact: Ajoute du responsive au menu navigation, menu burger, et aux boutons connexion et créer compte

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,5 +10,5 @@ export class HeaderComponent  {
   title = 'Trouvez votre propres Laverie Ou recourez à nos services de pressing';
   imgMobile = '../assets/img-content/Mobile.png';
   imgLocalisation = '../assets/img-content/localisation-nglesson.png';
-  telecharger = 'Téléchargez FindDry';
+  telecharger = 'Localiser votre laverie';
 }
