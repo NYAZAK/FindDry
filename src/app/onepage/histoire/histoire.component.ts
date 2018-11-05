cfc27bd8d9cbb91e9ea1cb3a42a87c29028f769c
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-05 10:19:24 +0100

    Fix: corrige la srx des images

--- src/app/onepage/histoire/histoire.component.ts
+++ src/app/onepage/histoire/histoire.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./histoire.component.css']
 })
 export class HistoireComponent implements OnInit {
-  reseautage = '../assets/img-content/William_and_Mary_at_laundromat-min.jpg';
-  lavomatique = '../assets/img-content/Safe_Boreas_Prosafe_min.jpg';
-  experience = '../assets/img-content/laundry_washing_machines_housewife_household_laundry_shop_woman_funny_accident.jpeg';
+  reseautage = '../../assets/img-content/William_and_Mary_at_laundromat-min.jpg';
+  lavomatique = '../../assets/img-content/Safe_Boreas_Prosafe_min.jpg';
+  experience = '../../assets/img-content/laundry_washing_machines_housewife_household_laundry_shop_woman_funny_accident.jpeg';
   constructor() { }
 
   ngOnInit() {
