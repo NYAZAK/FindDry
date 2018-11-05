2bc7ecbacc07e932835b4e191afe0aace7f432c9
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-05 10:09:11 +0100

    Refact: ajoute les elements de navigations de l'application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './onepage/header/header.component';
 import { HistoireComponent } from './onepage/histoire/histoire.component';
 import { LocaliserComponent } from './onepage/localiser/localiser.component';
 import { ContactComponent } from './onepage/contact/contact.component';
+
+
+const Routes: Routes = [
+    {path: '', pathMatch: 'full', component: HeaderComponent},
+    {path: 'Accueil', component: HeaderComponent},
+    {path: 'APropos', component: HistoireComponent},
+    {path: 'Localiser', component: LocaliserComponent},
+    {path: 'Contact', component: ContactComponent}
+    // {path:  'connexion', component: ConnexionComponent},
+    // {path:  'creerCompte', component: CreateAccountComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +31,10 @@ import { ContactComponent } from './onepage/contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(Routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
