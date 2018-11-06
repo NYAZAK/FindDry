3dff48c7fbf63b8ab2503bbe6b3565afac5305ed
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-06 22:19:53 +0100

    Fct: Ajoute le controle par un reactive form

--- src/app/onepage/contact/contact.component.ts
+++ src/app/onepage/contact/contact.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  formContact: FormGroup;
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formContact = this.fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  postMessage() {
+    console.log('message posté');
   }
 
 }
