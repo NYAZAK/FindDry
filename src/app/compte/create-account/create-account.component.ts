2dc39d9a241973454f767ae79f65c087997398e5
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-06 10:17:30 +0100

    Fct: Ajoute le formulaire de connexion avec le reactive forms pour le contrôle de la validité du formulaire

--- src/app/compte/create-account/create-account.component.ts
+++ src/app/compte/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
-
-  constructor() { }
+  title = 'Créez votre compte GRATUIT chez FindDry';
+  formCreate: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formCreate = this.formBuilder.group({
+      nom : ['', Validators.required],
+      prenom : ['', Validators.required],
+      mdp: ['', Validators.required],
+      mdp2: ['', Validators.required],
+      email : ['', Validators.required],
+      email2 : ['', Validators.required]
+    });
+  }
+
+  CreateCount() {
+
+    if (this.formCreate.valid) {
+      console.log('compte creer');
+    } else {
+      console.log('form pas valid');
+    }
   }
 
 }
