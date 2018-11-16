e39b647154cec7b2853a6298c893320d5cb95e03
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-16 22:28:18 +0100

    no message

--- src/app/onepage/contact.service.ts
+++ src/app/onepage/contact.service.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
 
-import {
-  AngularFireDatabase,
+import {AngularFireDatabase,
   // FirebaseListObservable 
 } from 'angularfire2/database';
 // import { AngularFireAuth } from 'angularfire2/auth';
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ContactService {
 
-  constructor(private afdb: AngularFireDatabase) { }
-
-
+  constructor(public afdb: AngularFireDatabase) { }
   sendMessage(message) {
-    return this.afdb.list('Message').push(message);
+    return this.afdb.list('MessageContacts').push(message);
   }
 }
