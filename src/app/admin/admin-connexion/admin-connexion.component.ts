999b454fdb07654d9d0e2776d253971979fe90d3
NYA Zakaria <zakaria.nya@hetic.net>
2018-11-25 10:57:16 +0100

    fct: ajoute les pages d'administrateur

--- src/app/admin/admin-connexion/admin-connexion.component.ts
+++ src/app/admin/admin-connexion/admin-connexion.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '../admin.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-connexion',
   templateUrl: './admin-connexion.component.html',
   styleUrls: ['./admin-connexion.component.css']
 })
 export class AdminConnexionComponent implements OnInit {
-
-  constructor() { }
+isAdmin: boolean = false;
+title= 'se connecter en tant qu\'administrateur'
+adminConnect: FormGroup;
+useridAd;
+  constructor(private formb: FormBuilder, private authAd: AdminService, 
+    private angularfa: AngularFireAuth, private route: Router) {
+    this.angularfa.authState.subscribe(user =>{
+      if(user) {
+        console.log(user);
+        this.useridAd = user.uid;
+      }
+    })
+   }
 
   ngOnInit() {
+    this.adminConnect = this.formb.group({
+      email: ['', [Validators.required, Validators.email]],
+      mdp: ['', [Validators.required, Validators.pattern(/[a-zA-Z0-9 ]{6,}/)]],
+    });
+    this.authAd.user$.subscribe(user => {
+      console.log(user, 'je suis un admin');
+      if(user && user.email === 'assorelik@gmail.com' && this.useridAd === "8tbwjECfYsTsGb9RsCsKlgDN1up2"){
+        console.log(user, 'je suis admin');
+        this.adconnexion();
+        this.isAdmin = true;
+      } else {
+        this.isAdmin = false;
+      }
+    });
+  }
+  adconnexion(){
+      const email = this.adminConnect.get('email').value;
+      const mdp = this.adminConnect.get('mdp').value;
+      this.useridAd; 
+      console.log(this.useridAd, 'connexion useruid');
+       if(email == 'assorelik@gmail.com' ) {
+        this.authAd.adLogin(email, mdp)
+        .then(() => {
+          this.isAdmin = true;
+        console.log('votre compte');
+        this.route.navigate(['/Adminview']);
+      })
+      .catch(err =>  { 
+        console.error('erreur : (', err.message);
+      });
+   
+    }
   }
 
 }
--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import * as firebase from 'firebase';
+import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  user$ : Observable<firebase.User> = null;
+  profils$;
+  msgContacts;
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  constructor(private angularfa : AngularFireAuth, private afdb: AngularFireDatabase) { 
+    this.user$ = angularfa.authState;
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      ) )
+  }
+
+  adLogin(email: string, password: string) {
+    return this.angularfa.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  adSingOut(){
+    this.angularfa.auth.signOut();
+  }
+
+  getProfilsOfMyUsers() {
+      return this.profils$ = this.afdb.list(`UserProfil/`).valueChanges();
+  }
+
+  getAllMessagesContacts() {
+    return this.msgContacts = this.afdb.list('MessageContacts').valueChanges();
+  }
 }
